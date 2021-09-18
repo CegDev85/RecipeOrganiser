5c454f0a762e39d67bc77db240681dad4ffcdba4
Craig Rankin <crankin85@hotmail.co.uk>
2021-09-18 15:00:35 +0100

    onion and garlic searches added

--- Home.js
+++ Home.js
@@ -11,17 +11,17 @@ const recipes = [
         machine: 6,
         ingredients:
             {   
-                salt:100,
-                sugar:40,
-                carmine:99,
+                salt:100.00,
+                sugar:40.00,
+                carmine:99.00,
                 // mustard:5,
-                // milkPowder:50.99
+                milkPowder:50.99
             }
         
     },
     {
         name:"lemon salmon rub",
-        size:1000,
+        size:1000.00,
         machine:14,
         ingredients:
             {
@@ -29,7 +29,25 @@ const recipes = [
                 lemonGranules:46.95,
                 brownSugar:200.34,
                 // milkPowder:50.99
-                eggPowder:68.90
+                eggPowder:68.90,
+                garlic:23.00,
+                onion:69.00
+            }
+        
+    },
+    {
+        name:"spicy morrocan seas",
+        size:198.00,
+        machine:6,
+        ingredients:
+            {
+                salt:200.00,
+                lemonGranules:46.95,
+                brownSugar:200.34,
+                // milkPowder:50.99
+                eggPowder:68.90,
+                carmine:45.86,
+                onion:50.00
             }
         
     }
@@ -62,6 +80,33 @@ const allergenSearch = () => {
 }
 
 console.log(allergenSearch())
+
+const onionSearch = () => {
+    const jobsWithOnion = []
+    for(recipe of recipes){
+        const items = Object.keys(recipe.ingredients)
+        if(items.includes("onion")){
+            jobsWithOnion.push(recipe)
+        }
+
+    }
+    return jobsWithOnion
+}
+console.log(onionSearch())
+
+const garlicSearch = () => {
+    const jobsWithGarlic = []
+    for(recipe of recipes){
+        const items = Object.keys(recipe.ingredients)
+        if(items.includes("garlic")){
+            jobsWithGarlic.push(recipe)
+        }
+    }
+    return jobsWithGarlic
+}
+console.log(garlicSearch())
+
+
 
 
 
